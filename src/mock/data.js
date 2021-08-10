4921a0444ef3261067a7adb56e3234d225bb3a15
nicokest <nicolask10@gmail.com>
2021-08-10 17:20:48 -0300

    1st content changes

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,28 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Nicolás Kestelboim | Front End Developer',
+  lang: 'en, es',
+  description: 'Web Developer specialized in React',
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, I am',
+  name: 'Nicolás Kestelboim',
+  subtitle: 'and I love Web Development',
+  cta: 'Send me a message',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
+  img: 'kestel.jpg',
+  paragraphOne:
+    'Front-End Developer Jr. con experiencia desarrollando sitios web responsive. Durante los últimos dos años trabajé de forma freelance en el desarrollo de sitios para pequeños y medianos clientes. Conocimientos en las siguientes tecnologías y lenguajes: HTML, CSS, Sass, Javascript, Ruby on Rails, React, GatsbyJS, GraphQL, MySQL, Wordpress y Shopify.',
+  paragraphTwo:
+    'Anteriormente como Social Media Manager participé en el desarrollo e implementación de estrategias digitales a través de contenido web, email marketing y redes sociales, para las siguientes marcas: Personal, Arnet, Seguros La Caja, Experta ART, Actimel, Laboratorios ELEA, Consejo Interamericano de Comercio y Producción, Rust-Oleum Latinoamérica, GlobalLogic, Guajira Viajes y Turismo.',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://www.linkedin.com/in/nicolaskestelboim/',
 };
 
 // PROJECTS DATA
@@ -57,9 +59,9 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Do you have a web project?',
+  btn: 'Write me',
+  email: 'nicolas.kestelboim@gmail.com',
 };
 
 // FOOTER DATA
